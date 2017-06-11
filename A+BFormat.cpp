b271aa5280805d1a54f7ac1e8ebcf43c6ad082ea
InTheShadow <2625354807@qq.com>
2017-06-11 19:03:41 +0800

    createContent3

--- A+BFormat.cpp
+++ A+BFormat.cpp
@@ -5,11 +5,11 @@ int main(){
 	int a,b;
 	cin >> a >> b;
 	//string s = to_string(a+b);
-	int len = s.length();
+	// int len = s.length();
 	
-	for(int i = 0;i<len;i++){
-		cout << s[i];
-		if(s[i] == '-') continue;
-		if((i+1)%3 == len%3 && i != len-1) cout << ',';
-	}
+	// for(int i = 0;i<len;i++){
+	// 	cout << s[i];
+	// 	if(s[i] == '-') continue;
+	// 	if((i+1)%3 == len%3 && i != len-1) cout << ',';
+	// }
 }
